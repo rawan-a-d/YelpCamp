cc5d42fd8ee7c9d4ae5aae96017b5569ac4afaf3
Rawan Abou Dehn <rawan.ad7@gmail.com>
2017-09-29 00:05:54 +0000

    Make the basic routes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# YelpCamp
+#YelpCamp
+
+##Initial Setup
+* Add Landing Page
+* Add Campgrounds Page that lists all campgrounds
+
+Each Campground has:
+   * Name
+   * Image
---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res)=>{
+    res.render("landing");
+});
+
+app.get("/campgrounds", (req, res)=>{
+    let campgrounds = [
+        {name: "Cherry Hill Park", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
+        {name: "Live Oak Landing", image: "https://farm5.staticflickr.com/4083/4961648022_7fec214b35.jpg"},
+        {name: "Myers Lake", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"}
+    ]
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.listen(process.env.PORT, process.env.IP, ()=>{
+    console.log("The YelpCamp server has started");
+})
